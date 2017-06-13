08de82eb90106f54c91b11f5de08b28431ef9ba7
Cyberia 2045 <sagansociety86@gmail.com>
2017-06-12 23:06:43 -0400

    final commit

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
-	var clock = document.getElementsByClassName("clock")[0];
+var clock = document.getElementsByClassName("clock")[0];
 
-		function hexClock() {
+	function hexClock() {
+	
+		var d = new Date(); // Date Object
+		var h = d.getHours();
+		var m = d.getMinutes();
+		var s = d.getSeconds();
+	
+		if (h <= 9) {
+			h = "0" + h;
+		}
+		if (m <= 9) {
+			m = "0" + m;
+		}
+		if (s <= 9) {
+			s = "0" + s;
+		}
+
+		var color = "#" + h + m + s
+		document.body.style.background = color;
+		clock.innerHTML = color;
+
+		- // setTimeout is needed for the clock to work continuously without a refresh. This refreshes the clock once every second.
 		
-			var d = new Date(); // Date Object
-			var h = d.getHours();
-			var m = d.getMinutes();
-			var s = d.getSeconds();
-		
-			if (h <= 9) {
-				h = "0" + h;
-			}
-			if (m <= 9) {
-				m = "0" + m;
-			}
-			if (s <= 9) {
-				s = "0" + s;
-			}
+	}; // closes hexClock
 
-			var color = "#" + h + m + s
-			document.body.style.background = color;
-			clock.innerHTML = color;
-
-			setTimeout(hexClock, 1000) // setTimeout is needed for the clock to work continuously without a refresh. This refreshes the clock once every second.
-			
-		}; // closes hexClock
-
-		hexClock();
+	hexClock();
